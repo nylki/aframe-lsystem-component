444e4ea160ac8863638f0829b3ea99d5fcbaf9f4
Tom <tombrwe@gmail.com>
2016-07-19 01:02:35 +0200

    Updates

--- libs/aframe-lsystem-component.js
+++ libs/aframe-lsystem-component.js
@@ -105,7 +105,6 @@
 	    },
 	    
 	    angle: {
-	      type: 'number',
 	      default: 90.0
 	    },
 	    
@@ -226,144 +225,30 @@
 	    //
 	    
 	    let self = this;
-	    
+
 	    this.el.removeObject3D('mesh');
 	    
 	    if(this.segmentElementGroupsMap !== undefined) {
 	      for (let segmentElGroup of this.segmentElementGroupsMap.values()) {
+	    
 	        segmentElGroup.removeObject3D('mesh');
-	        }
-	    }
-	    this.el.innerHTML = '';
-	    
-	    // Map for remembering the elements holding differnt segment types
-	    this.segmentElementGroupsMap = new Map();
-
-
-	    // The main segment used for saving transformations (rotation, translation, scale(?))
-	    this.transformationSegment = new THREE.Object3D();
-
-	    
-	    // We push copies of this.transformationSegment on branch symbols inside this array.
-	    this.stack = [];
-	    
-	    this.mixinMap = new Map();
-	    // Construct a map with keys = `symbol + colorIndex` from data.segmentMixins
-	    for (let [symbol, mixinList] of this.data.segmentMixins) {
-	      for (let i = 0; i < mixinList.length; i++) {
-	        this.mixinMap.set(symbol + i, mixinList[i]);
+	        segmentElGroup.innerHTML = '';
 	      }
 	    }
 	    
-	    // Map for buffering geometries for use in pushSegments()
-	    // when merging geometries ourselves and not by appending a `mixin` attributes,
-	    // as done with `mergeGeometry = false`.
-	    this.segmentObjects3DMap = new Map();
-	    
-	    this.segmentLengthMap = new Map();
-	    this.mergeGroups = new Map();
-	    
-	    let loadingPromises = [];
-	    
-	    
-	    // Collect mixin info by pre-appending segment elements with their mixin
-	    // Then ise the generated geometry etc.
-	    if(this.data.segmentMixins && this.data.segmentMixins.length !== 0) {
+	    if(oldData.angle && oldData.angle !== this.data.angle) {
+	      this.updateTurtleGraphics();
+	    } else if(oldData.axiom === undefined || (oldData.axiom && oldData.axiom !== this.data.axiom) || (oldData.productions && JSON.stringify(oldData.productions) !== JSON.stringify(this.data.productions))) {
+	      console.log('other stuff');
+	      // the following are async:
+	      this.updateLSystem();
+	      this.updateSegmentMixins();
 	      
-	      // Go through every symbols segmentMixins as defined by user
-	      for (let [symbol, mixinList] of this.data.segmentMixins) {
-	        
-	        // Set final functions for each symbol that has a mixin defined
-	        this.LSystem.setFinal(symbol, () => {self.pushSegment.bind(self, symbol)()});
-	        
-	        // And iterate the MixinList to buffer the segments or calculate segment lengths…
-	        for (let i = 0; i < mixinList.length; i++) {
-	          
-	          let mixinColorIndex = i;
-	          let mixin = mixinList[mixinColorIndex];
-	          
-	          loadingPromises.push(new Promise((resolve, reject) => {
-	            // Save mixinColorIndex for async promise below.
-
-	            let segmentElGroup = document.createElement('a-entity');
-	            segmentElGroup.setAttribute('id', mixin + '-group-' + mixinColorIndex + Math.floor(Math.random() * 10000));
-	            
-	            
-	            // TODO: Put it all under this.mergeData
-	            segmentElGroup.setAttribute('geometry', 'buffer', false);
-	            segmentElGroup.setAttribute('mixin', mixin);
-	            segmentElGroup.addEventListener('loaded', function (e) {
-	              
-	              let segmentObject = segmentElGroup.getObject3D('mesh').clone();
-	              
-	              // Make sure the geometry is actually unique
-	              // AFrame sets the same geometry for multiple entities. As we modify
-	              // the geometry per entity we need to have unique geometry instances.
-	              segmentElGroup.getObject3D('mesh').geometry.dispose();
-	              segmentObject.geometry = (segmentObject.geometry.clone());
-	              
-	              segmentLength = self.calculateSegmentLength(mixin, segmentObject.geometry);
-	              
-	              // Do some additional stuff like buffering 3D objects / geometry
-	              // if we want to merge geometries.
-	              if(self.data.mergeGeometries === true) {
-	                
-	                // Offset geometry by half segmentLength to get the rotation point right.
-
-	                let translation = self.data.translateAxis.clone().multiplyScalar((segmentLength * self.segmentLengthFactor)/2);
-	                segmentObject.geometry.applyMatrix( new THREE.Matrix4().makeTranslation( translation.x, translation.y, translation.z ) );
-
-	                
-	                let mergeGroup = new THREE.Mesh(
-	                  new THREE.Geometry(), segmentElGroup.getObject3D('mesh').material.clone()
-	                );
-	                
-	                self.segmentObjects3DMap.set(symbol + mixinColorIndex, segmentObject );
-	                self.mergeGroups.set(symbol + mixinColorIndex, mergeGroup);
-	              }
-	              
-	              segmentElGroup.removeObject3D('mesh');
-	              resolve();
-	            });
-	            
-	            
-	            if(this.segmentElementGroupsMap.has(symbol + mixinColorIndex)) {
-	              let previousElGroup = this.segmentElementGroupsMap.get(symbol + mixinColorIndex);
-	              this.segmentElementGroupsMap.delete(symbol + mixinColorIndex);
-	              this.el.removeChild(previousElGroup);
-	            }
-	            
-	            this.segmentElementGroupsMap.set(symbol + mixinColorIndex, segmentElGroup);
-	            this.el.appendChild(segmentElGroup);
-	            
-	            
-	          }));
-	        }
-	      }
 	    }
+
 	    
-
-
-	    // post params to worker
-	    // After all groups have been created, post to the worker.
-	    Promise.all(loadingPromises).then(() => {
-	      
-	      let params = {
-	        axiom: 		this.data.axiom,
-	        productions: this.data.productions,
-	        iterations: this.data.iterations
-	      }
-	      
-	      if(Date.now() - this.worker.startTime > 1000 ) {
-	        // if we got user input, but worker is running for over a second
-	        // terminate old worker and start new one.
-	        this.worker.terminate();
-	        this.initWorker();
-	      }
-	      
-	      this.worker.startTime = Date.now();
-	      this.worker.postMessage(params);
-	    })
+	    
+	    
 
 	  },
 	  
@@ -393,8 +278,6 @@
 	    let cappedColorIndex = Math.min(this.colorIndex, this.data.segmentMixins.get(symbol).length-1);
 	    
 	    let mixin = this.mixinMap.get(symbol + cappedColorIndex);
-	    
-
 	    
 	    if(this.data.mergeGeometries === false) {
 	      let newSegment = document.createElement('a-entity');
@@ -428,52 +311,189 @@
 	    this.transformationSegment.translateOnAxis(this.data.translateAxis, segmentLength * this.segmentLengthFactor);
 	  },
 	  
-	  updateTurtleGraphics: function() {
-
-			this.colorIndex = 0;
-	    this.lineWidth = 0.0005;
-	    this.lineLength = 0.125;
+	  updateLSystem: function () {
+	    let self = this;
 	    
-	    let angle = this.data.angle;
+	    // post params to worker
+	    let params = {
+	      axiom: 		this.data.axiom,
+	      productions: this.data.productions,
+	      iterations: this.data.iterations
+	    }
+	      
+	    if(Date.now() - this.worker.startTime > 1000 ) {
+	      // if we got user input, but worker is running for over a second
+	      // terminate old worker and start new one.
+	      this.worker.terminate();
+	      this.initWorker();
+	    }
+	      
+	    this.worker.startTime = Date.now();
+	    this.worker.postMessage(params);
+	  },
+	  
+	  updateSegmentMixins: function () {
+	    let self = this;
 	    
-			// Set quaternions based on angle slider
-			this.xPosRotation.setFromAxisAngle( this.X, (Math.PI / 180) * angle );
-			this.xNegRotation.setFromAxisAngle( this.X, (Math.PI / 180) * -angle );
-			
-			this.yPosRotation.setFromAxisAngle( this.Y, (Math.PI / 180) * angle );
-			this.yNegRotation.setFromAxisAngle( this.Y, (Math.PI / 180) * -angle );
-			this.yReverseRotation.setFromAxisAngle( this.Y, (Math.PI / 180) * 180 );
-			
-			this.zPosRotation.setFromAxisAngle( this.Z, (Math.PI / 180) * angle );
-			this.zNegRotation.setFromAxisAngle( this.Z, (Math.PI / 180) * -angle );
-			//
-			// this.geometry = new THREE.CylinderGeometry(this.lineWidth, this.lineWidth, self.data.lineLength, 3);
-			// this.geometry.rotateZ((Math.PI / 180) * 90);
-			// this.geometry.translate( -(this.data.segmentLength/2), 0, 0 );
-			// for (let face of this.geometry.faces) {
-			// 	face.color.setHex(this.colors[colorIndex]);
-			// }
-			// this.geometry.colorsNeedUpdate = true;
+	    this.el.innerHTML = '';
+	    
+	    // Map for remembering the elements holding differnt segment types
+	    this.segmentElementGroupsMap = new Map();
 
-			this.LSystem.final();
-	    // finally set the merged meshes to be visible.
-	    if(this.data.mergeGeometries === true) {
-	      for (let tuple of this.segmentElementGroupsMap) {
-	        let [symbolWithColorIndex, elGroup] = tuple;
+	    
+	    this.mixinMap = new Map();
+	    // Construct a map with keys = `symbol + colorIndex` from data.segmentMixins
+	    for (let [symbol, mixinList] of this.data.segmentMixins) {
+	      for (let i = 0; i < mixinList.length; i++) {
+	        this.mixinMap.set(symbol + i, mixinList[i]);
+	      }
+	    }
+	    
+	    // Map for buffering geometries for use in pushSegments()
+	    // when merging geometries ourselves and not by appending a `mixin` attributes,
+	    // as done with `mergeGeometry = false`.
+	    this.segmentObjects3DMap = new Map();
+	    
+	    this.segmentLengthMap = new Map();
+	    this.mergeGroups = new Map();
+	    
+	    this.mixinPromises = [];
+	    
+	    
+	    // Collect mixin info by pre-appending segment elements with their mixin
+	    // Then ise the generated geometry etc.
+	    if(this.data.segmentMixins && this.data.segmentMixins.length !== 0) {
+	      
+	      // Go through every symbols segmentMixins as defined by user
+	      for (let [symbol, mixinList] of this.data.segmentMixins) {
+	        
+	        // Set final functions for each symbol that has a mixin defined
+	        this.LSystem.setFinal(symbol, () => {self.pushSegment.bind(self, symbol)()});
+	        
+	        // And iterate the MixinList to buffer the segments or calculate segment lengths…
+	        for (let i = 0; i < mixinList.length; i++) {
+	          
+	          let mixinColorIndex = i;
+	          let mixin = mixinList[mixinColorIndex];
+	          
+	          self.mixinPromises.push(new Promise((resolve, reject) => {
+	            // Save mixinColorIndex for async promise below.
 
-	        let mergeGroup = this.mergeGroups.get(symbolWithColorIndex);
-	        // Remove unused element groups inside our element
-	        if(mergeGroup.geometry.vertices.length === 0) {
-	          this.el.removeChild(elGroup);
-	        } else {
-	          elGroup.setObject3D('mesh', this.mergeGroups.get(symbolWithColorIndex));
-	          elGroup.setAttribute('mixin', this.mixinMap.get(symbolWithColorIndex));
+	            let segmentElGroup = document.createElement('a-entity');
+	            segmentElGroup.setAttribute('id', mixin + '-group-' + mixinColorIndex + Math.floor(Math.random() * 10000));
+	            
+	            
+	            // TODO: Put it all under this.mergeData
+	            segmentElGroup.setAttribute('geometry', 'buffer', false);
+	            segmentElGroup.setAttribute('mixin', mixin);
+	            segmentElGroup.addEventListener('loaded', function (e) {
+	              
+	              let segmentObject = segmentElGroup.getObject3D('mesh').clone();
+	              
+	              // Make sure the geometry is actually unique
+	              // AFrame sets the same geometry for multiple entities. As we modify
+	              // the geometry per entity we need to have unique geometry instances.
+	              segmentElGroup.getObject3D('mesh').geometry.dispose();
+	              segmentObject.geometry = (segmentObject.geometry.clone());
+	              
+	              segmentLength = self.calculateSegmentLength(mixin, segmentObject.geometry);
+	              
+	              // Do some additional stuff like buffering 3D objects / geometry
+	              // if we want to merge geometries.
+	              if(self.data.mergeGeometries === true) {
+	                
+	                // Offset geometry by half segmentLength to get the rotation point right.
+
+	                let translation = self.data.translateAxis.clone().multiplyScalar((segmentLength * self.segmentLengthFactor)/2);
+	                segmentObject.geometry.applyMatrix( new THREE.Matrix4().makeTranslation( translation.x, translation.y, translation.z ) );
+
+	                self.segmentObjects3DMap.set(symbol + mixinColorIndex, segmentObject );
+
+	              }
+	              
+	              segmentElGroup.removeObject3D('mesh');
+	              resolve();
+	            });
+	            
+	            
+	            if(this.segmentElementGroupsMap.has(symbol + mixinColorIndex)) {
+	              let previousElGroup = this.segmentElementGroupsMap.get(symbol + mixinColorIndex);
+	              this.segmentElementGroupsMap.delete(symbol + mixinColorIndex);
+	              this.el.removeChild(previousElGroup);
+	            }
+	            
+	            this.segmentElementGroupsMap.set(symbol + mixinColorIndex, segmentElGroup);
+	            this.el.appendChild(segmentElGroup);
+	            
+	            
+	          }));
 	        }
 	      }
 	    }
+	  },
+	  
+	  updateTurtleGraphics: function() {
+	    Promise.all(this.mixinPromises).then(() => {
+	      // The main segment used for saving transformations (rotation, translation, scale(?))
+	      this.transformationSegment = new THREE.Object3D();
 
+	      // set merge groups
+	      if(this.data.mergeGeometries === true)
+	      for (let [id, segmentObject] of this.segmentObjects3DMap) {
+	        this.mergeGroups.set(id, new THREE.Mesh(
+	          new THREE.Geometry(), segmentObject.material
+	        ));
+	        
+	      }
+	      
+	      
+	      // We push copies of this.transformationSegment on branch symbols inside this array.
+	      this.stack = [];
+	      
+	      this.colorIndex = 0;
+	      this.lineWidth = 0.0005;
+	      this.lineLength = 0.125;
+	      
+	      let angle = this.data.angle;
+	      
+	      // Set quaternions based on angle slider
+	      this.xPosRotation.setFromAxisAngle( this.X, (Math.PI / 180) * angle );
+	      this.xNegRotation.setFromAxisAngle( this.X, (Math.PI / 180) * -angle );
+	      
+	      this.yPosRotation.setFromAxisAngle( this.Y, (Math.PI / 180) * angle );
+	      this.yNegRotation.setFromAxisAngle( this.Y, (Math.PI / 180) * -angle );
+	      this.yReverseRotation.setFromAxisAngle( this.Y, (Math.PI / 180) * 180 );
+	      
+	      this.zPosRotation.setFromAxisAngle( this.Z, (Math.PI / 180) * angle );
+	      this.zNegRotation.setFromAxisAngle( this.Z, (Math.PI / 180) * -angle );
+	      //
+	      // this.geometry = new THREE.CylinderGeometry(this.lineWidth, this.lineWidth, self.data.lineLength, 3);
+	      // this.geometry.rotateZ((Math.PI / 180) * 90);
+	      // this.geometry.translate( -(this.data.segmentLength/2), 0, 0 );
+	      // for (let face of this.geometry.faces) {
+	      // 	face.color.setHex(this.colors[colorIndex]);
+	      // }
+	      // this.geometry.colorsNeedUpdate = true;
 
-		},
+	      this.LSystem.final();
+	      // finally set the merged meshes to be visible.
+	      if(this.data.mergeGeometries === true) {
+	        for (let tuple of this.segmentElementGroupsMap) {
+	          let [symbolWithColorIndex, elGroup] = tuple;
+	          
+	          let mergeGroup = this.mergeGroups.get(symbolWithColorIndex);
+	          // Remove unused element groups inside our element
+	          if(mergeGroup.geometry.vertices.length === 0) {
+	            this.el.removeChild(elGroup);
+	          } else {
+	            elGroup.setObject3D('mesh', this.mergeGroups.get(symbolWithColorIndex));
+	            elGroup.setAttribute('mixin', this.mixinMap.get(symbolWithColorIndex));
+	          }
+	        }
+	      }
+	      
+	    });
+	  },
 	  
 	  
 	  onWorkerUpdate: function(e) {
